fcac08a4023e092537a321eb708f674a822a342d
Khahory <angeld05@live.com>
2023-02-15 23:18:58 -0400

    formatting code...

--- frontend/src/services/server.js
+++ frontend/src/services/server.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const BASE_URL = 'http://localhost:4444/backend/api/v1';
 
 const fetchStatus = async () => {
-  const response = await axios.get(`${BASE_URL}/Welcome`);
+  const response = await axios.get(`${BASE_URL}/welcome`);
   return response.data;
 };
 

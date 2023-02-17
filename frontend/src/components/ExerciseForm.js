1917914bf9627beddbf13d27b57d375fae2c1dfb
Khahory <angeld05@live.com>
2023-02-16 22:45:43 -0400

    best way getting data

--- frontend/src/components/ExerciseForm.js
+++ frontend/src/components/ExerciseForm.js
@@ -1,28 +1,52 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import PostExerciseForm from "../services/PostExerciseForm";
 
 const ExerciseForm = () => {
+    // my state variables
+    const [exercise, setExercise] = useState({
+        bucketX: "",
+        bucketY: "",
+        amountZ: "",
+    });
 
     // post request to backend
     const handleSubmit = (event) => {
-        alert('handleSubmit');
+        PostExerciseForm({
+            bucket_x: exercise.bucketX,
+            bucket_y: exercise.bucketY,
+            amount_wanted_z: exercise.amountZ,
+        }).then((response) => {
+            console.log(response);
+        });
         event.preventDefault(); // Prevents the default action of submitting the form
+    }
+
+    // handle change in input
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+        setExercise((prevExercise) => {
+            return {
+                ...prevExercise,
+                [name]: value,
+            }
+        });
     }
 
     return (
         <form onSubmit={handleSubmit}>
             <label>
                 Bucket x:
-                <input type="text"/>
+                <input type="text" name="bucketX" onChange={handleChange} />
             </label>
             <br/>
             <label>
                 Bucket Y:
-                <input type="text"/>
+                <input type="text" name="bucketY" onChange={handleChange} />
             </label>
             <br/>
             <label>
                 Amount wanted Z:
-                <input type="text"/>
+                <input type="text" name="amountZ" onChange={handleChange} />
             </label>
             <br/><br/>
             <input type="submit" value="Submit"/>
--- frontend/src/services/PostExerciseForm.js
+++ frontend/src/services/PostExerciseForm.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 import BASE_URL from '../config';
 
-const PostExerciseForm = async () => {
+const PostExerciseForm = async ({bucket_x, bucket_y, amount_wanted_z}) => {
     const response = await axios.post(`${BASE_URL}/exercise`, {
         exercise: {
-            bucket_x: '2',
-            bucket_y: '10',
-            amount_wanted_z: '4',
+            bucket_x,
+            bucket_y,
+            amount_wanted_z,
         },
     });
     return response.data;

34caa933871c3764c1e2feafc8b02d8c168ecab9
max - angel <angeld05@live.com>
2023-02-17 13:19:32 -0400

    we need number 0

--- frontend/src/helpers/Form.jsx
+++ frontend/src/helpers/Form.jsx
@@ -1,6 +1,6 @@
 // This function is used to format the input of the user
 const formatingInput = (value) => {
-    const regex = /^[1-9\b]+$/; // only allow numbers
+    const regex = /^[0-9\b]+$/; // only allow numbers
     if (value === '' || regex.test(value)) {
         return value;
     }

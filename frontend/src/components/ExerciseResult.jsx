9ef429110e575d5b055372e28e0766b0422979b0
Khahory <angeld05@live.com>
2023-02-18 12:20:09 -0400

    compare the solutions works

--- frontend/src/components/ExerciseResult.jsx
+++ frontend/src/components/ExerciseResult.jsx
@@ -2,6 +2,24 @@ import React from "react";
 
 const ExerciseResult = (props) => {
     const {result} = props;
+
+    // some validation
+    if (!result.status || !result.is_valid)
+        return (<h3>Something goes wrong, reload the page</h3>)
+
+    // get the solution from the result
+    let {res_x, res_y} = result.data;
+    res_x = res_x.is_done ? res_x : null;
+    res_y = res_y.is_done ? res_y : null;
+
+    // compare the solutions
+    const best_solution = res_x && res_y ?
+        (res_x.step_times < res_y.step_times ? res_x : res_y) :
+        (res_x ? res_x : res_y);
+
+    console.warn(best_solution)
+
+
     return (
         <div>
             <h3>Result</h3>
